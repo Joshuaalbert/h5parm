aebcddc6bfcb84efe34096bbd98c20ebdbfe544b
joshuaalbert <albert@strw.leidenuniv.nl>
2020-11-27 11:59:50 +0100

    * pip compliance

--- setup.py
+++ setup.py
@@ -7,14 +7,19 @@ __maximum_numpy_version__ = '1.19.0'
 __minimum_tables_version__ = '3.6.1'
 __minimum_astropy_version__ = '4.0.0'
 
-
 setup_requires = ['numpy<' + __maximum_numpy_version__,
                   'tables>=' + __minimum_tables_version__,
                   'astropy>=' + __minimum_astropy_version__]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='h5parm',
       version='0.0.1',
       description='H5Parm data pack interface',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joshuaalbert/h5parm",
       author=['Joshua G. Albert'],
       author_email=['albert@strw.leidenuniv.nl'],
       setup_requires=setup_requires,
@@ -23,5 +28,11 @@ setup(name='h5parm',
       ],
       package_data={'h5parm': ['arrays/*']},
       package_dir={'': './'},
-      packages=find_packages('./')
+      packages=find_packages('./'),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
       )
