76bdcb1ab1aabbe6673792fb561e4a54f104d1b6
joshuaalbert <albert@strw.leidenuniv.nl>
2020-11-27 12:01:53 +0100

    * pip compliance

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(name='h5parm',
       packages=find_packages('./'),
       classifiers=[
           "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
+          "License :: OSI Approved :: Apache Software License",
           "Operating System :: OS Independent",
       ],
       python_requires='>=3.6',
