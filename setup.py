58ce27fbd0b68ed1cdfe9d4d8c9a9a59580bb129
joshuaalbert <albert@strw.leidenuniv.nl>
2021-06-01 21:08:10 -0400

    * 0.0.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='h5parm',
-      version='0.0.4',
+      version='0.0.5',
       description='H5Parm data pack interface',
       long_description=long_description,
       long_description_content_type="text/markdown",
