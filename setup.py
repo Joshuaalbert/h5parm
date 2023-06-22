af1b44bbc651bd3752c506aeca11d26754a97ebc
joshuaalbert <albert@strw.leidenuniv.nl>
2023-06-22 19:46:46 +0200

    * Bump to 1.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='h5parm',
-      version='1.0.4',
+      version='1.0.5',
       description='H5Parm data pack interface',
       long_description=long_description,
       long_description_content_type="text/markdown",
